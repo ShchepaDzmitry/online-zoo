215e4633d27e67cc5f937fcb26a3384e6dd7fde9
ShchepaDzmitry <shchapiorka.dzmitry@gmail.com>
2026-03-15 18:30:59 +0100

    fix: linter rules for deployment

--- src/utils/carouselUtils.ts
+++ src/utils/carouselUtils.ts
@@ -3,10 +3,10 @@ import { Pet } from "../pages/landing/interfaces/pet";
 
 export type CarouselItem = Pet | Feedback;
 
-export const renderCarouselArray = (carouselArray: Array<Pet | Feedback>, carouselContainerElement: HTMLElement, renderFn: (item: CarouselItem, selectedItem?: unknown) => string, selectedItem?: unknown): void => {
-    carouselContainerElement.innerHTML = carouselArray.map((carouselItem: (Pet | Feedback)) => renderFn(carouselItem, selectedItem)).join('');
+export const renderCarouselArray = <T extends CarouselItem>(carouselArray: Array<T>, carouselContainerElement: HTMLElement, renderFn: (item: T, selectedItem?: unknown) => string, selectedItem?: unknown): void => {
+    carouselContainerElement.innerHTML = carouselArray.map((carouselItem: (T)) => renderFn(carouselItem, selectedItem)).join('');
 };
   
-export const moveRight = (arr: Array<Pet | Feedback>): (Array<Pet | Feedback>) => [...arr.slice(1), arr[0]];
+export const moveRight = <T extends CarouselItem>(arr: Array<T>): (Array<T>) => [...arr.slice(1), arr[0]];
   
-export const moveLeft = (arr: Array<Pet | Feedback>): Array<Pet | Feedback>  => [arr[arr.length - 1], ...arr.slice(0, -1)];
+export const moveLeft = <T extends CarouselItem>(arr: Array<T>): Array<T>  => [arr[arr.length - 1], ...arr.slice(0, -1)];
