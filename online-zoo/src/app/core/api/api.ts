e24b2ed2622063c0bf24cd402301b55a874904b4
ShchepaDzmitry <shchapiorka.dzmitry@gmail.com>
2026-03-29 18:56:28 +0200

    feat: add environment variables

--- online-zoo/src/app/core/api/api.ts
+++ online-zoo/src/app/core/api/api.ts
@@ -2,12 +2,13 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable, catchError, throwError } from 'rxjs';
 import { ILoginForm, IRegistrationForm } from '../auth/auth.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Api {
-  private INITIAL_PATH = 'https://vsqsnqnxkh.execute-api.eu-central-1.amazonaws.com/prod/';
+  private INITIAL_PATH = environment.apiUrl;
   private http = inject(HttpClient);
 
   get<T>(additionalPath: string = ''): Observable<T> {
